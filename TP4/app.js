c7d2093bfa7ac66681a9f26f09c73ef1e6d47773
Ginyto <agianlucca@gmail.com>
2021-05-06 19:53:48 +0200

    exo3 presque fini

--- TP4/app.js
+++ TP4/app.js
@@ -1,4 +1,9 @@
 var newUserForm = document.forms.newUser;
+var utilitab = document.getElementById("utilitab");
+var tachtab = document.getElementById("tachtab");
+var boutton_add = document.getElementById("submit");
+
+
 
 class Xelor {
     rolex = 0;
@@ -37,11 +42,12 @@ function addElem () {
                 
         newItem.append(firstnameTd, lastnameTd, emailTd, roleTd)
         /* le premier élément dans le document qui contient la classe "datatable" est retourné*/
-        const table = document.querySelector('.datatable tbody')
+        const table = document.querySelector('#tabutili tbody')
                 
         table.appendChild(newItem)
 
         chrono.rolex = 0;
+        chrono.aiguille = false
     }
     else{
         alert("Veuillez patienter")
@@ -51,23 +57,68 @@ function addElem () {
 
        
 function removeAll (){
-    const tbody = document.querySelector('.datatable tbody')
+
+    if (utilitab.hidden == false) {
+       const tbody = document.querySelector('#tabutili tbody')
            
-    while (tbody.firstChild){
-        tbody.removeChild(tbody.firstChild)
+        while (tbody.firstChild){
+            tbody.removeChild(tbody.firstChild)
+        } 
     }
+   else{
+        const tbody = document.querySelector('#tabtache tbody')
+        console.log("heho")
+        while (tbody.firstChild){
+            tbody.removeChild(tbody.firstChild)
+        } 
+    }
+    
 }
 
-function off(){
-    
-    
+function ShowUsers(){
+    utilitab.hidden = false;
+    tachtab.hidden = true;
 }
 
+function ShowTasks(){
+    utilitab.hidden = true;
+    tachtab.hidden = false;
+}
+
+function getTasks(){
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(function (data) {
+    console.log('data', data)})
+}
+
+function createTask(userID, id, titre, completed){
+
+                
+    const newItem = document.createElement('tr')
+    const useridTd = document.createElement('td')
+    const idTd = document.createElement('td')
+    const titreTd = document.createElement('td')
+    const doneTd = document.createElement('td')
+    useridTd.textContent = userID
+    idTd.textContent = id
+    titreTd.textContent = titre
+    doneTd.textContent = completed
+                
+    newItem.append(useridTd, idTd, titreTd, doneTd)
+    /* le premier élément dans le document qui contient la classe "datatable" est retourné*/
+    const table = document.querySelector('#tabtache tbody')
+                
+    table.appendChild(newItem)
+}
+
+getTasks();//le nombre de tache
 
 
-function main(){
-    var boutton_add = document.getElementById("submit");
+ShowTasks();
+createTask("faire", "de", "la", "rando")
 
+function tempo(){
     chrono.rolex += 1;
     console.log("seconde : "+chrono.rolex);
 
@@ -81,23 +132,8 @@ function main(){
         boutton_add.setAttribute("style", "background-color:#f5f5f5");
         boutton_add.innerHTML = "Ajouter";
     }
-
     
 }
 
-
-setInterval(main,1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
+setInterval(tempo,1000);
 
